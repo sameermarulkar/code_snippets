8955c7a2bf2915739f78e70747b4c2ad69ed4060
Sameer Marulkar <sameermarulkar@outlook.com>
2021-08-29 14:40:13 -0700

    Added comments
    1. can multiple routes point to the same function? yes
    2. how to run the program with flask run or python <.py>

--- Python/Flask_Blog/01-Getting-Started/flaskblog.py
+++ Python/Flask_Blog/01-Getting-Started/flaskblog.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__) 
 
 
 @app.route("/")
-@app.route("/home")
+@app.route("/home")             # both / and /home point to home()
 def home():
     return "<h1>Home Page</h1>"
 
@@ -13,5 +13,12 @@ def about():
     return "<h1>About Page</h1>"
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__':      # this is needed only if the program is run directly i.e. python flaskblog.py
+    app.run(debug=True)         # __name__ == 'module_name' if ran indirectly
+
+'''
+    to run this without if __name__ use: 
+        export FLASK_APP=flaskblog.py
+        export FLASK_DEBUG=1
+        flask run 
+'''
